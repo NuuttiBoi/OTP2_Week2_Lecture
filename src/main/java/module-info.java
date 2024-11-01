7be9770bd813efa85e460f97e3e700b6a564c9d5
NuuttiBoi <104026912+NuuttiBoi@users.noreply.github.com>
2024-11-01 17:53:07 +0900

    init

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module java.otp2week2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens otp2 to javafx.fxml;
+    exports otp2;
+}
